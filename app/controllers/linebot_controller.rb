b39b7ac2ba9e31a0123ca86f2e4ad135a7e90636
古屋哲人 <tetsundo@koyatetsutonoMacBook-puro.local>
2018-12-16 17:37:46 +0900

    enum

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -14,7 +14,7 @@ class LinebotController < ApplicationController
 
 		signature = request.env['HTTP_LINE_SIGNATURE']
 		unless client.validate_signature(body, signature)
-			eroor 400 do 'Bad Request' end
+			error 400 do 'Bad Request' end
 		end
 
 		events = client.parse_events_form(body)
